fdeafb3d108beedfc86fae16ef476307ffd978cf
ShowlaborM <122938824+ShowlaborM@users.noreply.github.com>
2023-02-23 05:52:21 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
-#check include "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
- * @n: check integer n
+ * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
