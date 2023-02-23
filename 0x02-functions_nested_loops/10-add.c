b7815459c5456a193ebda130a9d2540231222feb
Solebo Muritala Shina <meetengrshowlabor@gmail.com>
2023-02-22 20:04:22 -0800

    Concluding part C-function and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * adds two integers and returns the result
+ * takes integer a
+ * takes integer b
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * check for lowercase character
+ * In integerc
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
