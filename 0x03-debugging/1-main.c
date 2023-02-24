26d6a5950ddbb497e7f9ebf47f23ed6372de0125
Solebo Muritala Shina <meetengrshowlabor@gmail.com>
2023-02-24 14:06:50 -0800

    Correcting 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,15 +11,12 @@ int main(void)
 
         printf("Infinite loop incoming :(\n");
 
-        i = 0;
+        /**i = 0;
 
         while (i < 10)
         {
                 putchar(i); 
-		/** i++ is missing;
-		 * no increment for i so it will always be 0 
-		 */
-        }
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
