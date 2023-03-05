438712fc0379e61022bdc8fb690c5c5c5d680a77
Solebo Muritala Shina <meetengrshowlabor@gmail.com>
2023-03-04 20:42:16 -0800

    correctiong 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,30 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
- * @dest: input value1
- * @src: input value2
+ * @dest: input value
+ * @src: input value
  *
  * Return: void
  */
-char *_strcat(char *desk, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	i = 0
-	while (desk[i] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		desk[i] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	desk[i] =  '\0';
-	return (desk);
-
+	dest[i] = '\0';
+	return (dest);
 }
 
